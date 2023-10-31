18329f11f5cdd13798039c655aeba8a0018d5ceb
Arjun Maniyani <arjunrmaniyani@gmail.com>
2023-10-31 14:24:49 +0530

    updated header definition

--- arjun/arjun.h
+++ arjun/arjun.h
@@ -1,5 +1,7 @@
 #include "../definitions.h"
 
 void roll_list_of_each_class(tree_node_t* classes[], int num_classes);
-void balance_binary_tree()
 // TODO arjun - add function prototype for balancing tree here
+
+//Returns the root node
+tree_node_t* insert(tree_node_t* node, char *name, char *srn, float cgpa);
